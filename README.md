13ba29334e3fdef9d573f692f5c1071acf112850
gustavo auad piccoli <gustavo.a.piccoli@gmail.com>
2023-02-13 08:51:48 -0300

    versão mais atualizada, falta verificar big step

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-1) Para o typeinfer, acredito  que estão corretas as funções básicas, as extensões de pares, operações binárias e listas. Para just, nothing e match nothing, acredito estar errado.
-
-2) Para o avaliador big step, acredito que estão corretas as funções básicas, extensões de pares e operações binárias. As funções de listas, nothing, just e matchnothing, acredito estarem erradas.
-
-3) Também tenho que rever se a parte de valores está correta.
-
-4) Talvez mudar o sistema de valores, não sei se posso deixar direto como expressões
-
-5) Eu não utilizei o tipo maybe, então algo está errado
+1) Verificar se big step de ExTail está correto
+2) Fazer o big step de ExMatchList
+3) Talver alterar o big step de ExJust e ExNothing para fazer a avaliação das expressões internas
--- testes.ml
+++ testes.ml
@@ -1,5 +1,4 @@
 (* Testes de inferencia de tipo: *)
-
 (*Sem ambientes*)
 typeinfer [] (ExIf(ExTrue,ExFalse,ExTrue));;
 typeinfer [] (ExIf(ExTrue,(ExNum 3),(ExNum 4)));;
@@ -13,35 +12,65 @@ typeinfer [] (ExPair(ExTrue, ExFalse));;
 typeinfer [] (ExPair(ExTrue, ExNum(2)));;
 typeinfer [] (ExFst(ExPair(ExTrue, ExNum(2))));;
 typeinfer [] (ExSnd(ExPair(ExTrue, ExNum(2))));;
-
-(*Com Ambientes*)
+(*Com Ambientes*) (*Não esquecer de colocar o valor da variável entre parênteses*)
 typeinfer (update [] "x" TyInt) ExTrue;;
 typeinfer (update [] "x" TyInt) (ExVar "x");;
 typeinfer (update [] "x" TyBool) (ExIf (ExVar "x", ExTrue, ExFalse));;
 typeinfer (update(update(update [] "x" TyInt) "y" TyBool) "z" TyBool) (ExVar "x");;
 typeinfer (update(update(update [] "x" TyBool) "y" TyInt) "z" TyInt) (ExIf (ExVar "x", ExVar "y", ExVar "z"));;
 typeinfer (update(update(update [] "x" TyBool) "y" TyInt) "z" TyBool) (ExIf (ExVar "x", ExVar "y", ExVar "z"));; (*Erro*)
-
 (*Listas*)
-typeinfer [] (ExNil(TyBool));;
-typeinfer [] (ExNil(ExTrue));; (*Erro*)
-typeinfer [] (ExList(ExTrue, ExNil(TyBool)));;
-typeinfer [] (ExList(ExTrue, ExNil(TyInt)));; (*Erro*)
-typeinfer [] (ExList(ExTrue, ExList(ExFalse, ExNil(TyBool))));;
-typeinfer [] (ExList(ExTrue, ExList(ExFalse, ExNil(TyInt))));; (*Erro*)
-typeinfer [] (ExList(ExTrue, ExList(ExNum(2), ExNil(TyInt))));; (*Erro*)
-typeinfer [] (ExList(ExNum(1), ExList(ExNum(2), ExNil(TyInt))));;
-typeinfer [] (ExList(ExNum(1), ExList(ExNum(2), ExNum(4))));; (*Erro*)
-
-
-
-
+typeinfer [] (ExNil(TyList(TyBool)));;
+typeinfer [] (ExNil(TyBool));; (*Erro pois Nil tem que receber um tipo lista de um tipo*)
+typeinfer [] (ExList(ExTrue, ExNil(TyList(TyBool))));;
+typeinfer [] (ExList(ExTrue, ExNil(TyList(TyInt))));; (* Erro pois o tipo da lista de nil é diferente do tipo do head *)
+typeinfer [] (ExList(ExTrue, ExList(ExFalse, ExNil(TyList(TyBool)))));;
+typeinfer [] (ExList(ExTrue, ExList(ExFalse, ExNil(TyList(TyInt)))));; (* Erro pois o tipo da lista de nil é diferente do tipo do head *)
+typeinfer [] (ExList(ExNum(4), ExList(ExNum(3), ExNil(TyList(TyInt)))));; 
+typeinfer [] (ExList(ExTrue, ExList(ExNum(3), ExNil(TyList(TyInt)))));;  (* Erro pois a lista não tem consistencia de tipo *)
+(*Maybe*)
+typeinfer [] (ExJust(ExTrue));;
+typeinfer [] (ExJust(ExNil(TyList(TyBool))));;
+typeinfer [] (ExNothing(TyBool));;
 
 
 (* Testes de avaliacao: *)
-eval (ExNume(3));;
-eval (ExVar("x"));; (*Erro*)
-eval (ExTrue);;
-eval (ExIf(ExTrue, ExNum(3), ExNum(4)));;
-eval (ExBinop(And, ExTrue, ExFalse));;
-eval (ExIf(ExTrue, (ExBinop(And, ExTrue, ExFalse)), ExNum(4)));;
+eval [] (ExNum(3));;
+eval [] (ExTrue);;
+eval [] (VTrue);; (*Erro pois eval espera uma expressão e não um valor *)
+eval [] (ExFalse);;
+eval [] (ExVar("x"));; (*Erro pois a variável não foi declarada no ambiente*)
+eval (update [] "x" (ExTrue)) (ExVar("x"));; (*Erro pois a variável foi declarada como uma expressão e não um valor*)
+eval (update [] "x" (VTrue)) (ExVar("x"));;
+eval (update (update [] "x" (VTrue)) "y" (VFalse)) (ExVar("x"));;
+eval (update (update [] "x" (VTrue)) "y" (VFalse)) (ExVar("y"));;
+eval (update [] "x" (VNum(2))) (ExVar("x"));;
+eval [] (ExIf(ExTrue, ExNum(3), ExNum(4)));;
+eval (update [] "x" (VNum(3))) (ExIf(ExTrue, ExVar("x"), ExNum(4)));;
+eval [] (ExIf(ExTrue, (ExBinop(And, ExTrue, ExFalse)), ExNum(4)));;
+(* 
+eval [] (ExFn())
+eval [] (ExApp())
+eval [] (ExLet())
+eval [] (ExLetRec())
+ *)
+eval [] (ExBinop(And, ExTrue, ExFalse));;
+eval [] (ExBinop(Soma, ExNum(3), ExNum(4)));;
+eval (update [] "x" (VTrue)) (ExBinop(And, ExVar("x"), ExFalse));;
+eval [] (ExBinop(Multiplicacao, ExNum(3), ExNum(4)));;
+eval (update [] "x" (VNum(5))) (ExBinop(Multiplicacao, ExVar("x"), ExNum(4)));;
+eval [] (ExBinop(Divisao, ExNum(3), ExNum(4)));;
+eval [] (ExBinop(Divisao, ExNum(3), ExNum(0)));; (*Erro divisisão por zero *)
+eval [] (ExPar(ExTrue, ExFalse));;
+eval (update (update [] "x" (VTrue)) "y" (VFalse)) (ExPar(ExVar("x"), ExVar("y")));;
+
+
+
+
+
+
+
+
+
+
+
